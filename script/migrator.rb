123232836e371519d8adf68ca5939e6858ecc374
nkunguza <timothy@timothy-SVE15115FXS.(none)>
2014-06-03 10:07:15 +0200

    created general framework of migrator script

---
+++ script/migrator.rb
@@ -0,0 +1,50 @@
+def start
+
+end
+
+def create_patient
+
+end
+
+def create_guardian
+
+end
+
+def create_visit_encounter
+
+end
+
+def create_outcome
+
+end
+
+def create_first_visit_encounter
+
+end
+
+def create_hiv_staging_encounter
+
+end
+
+def create_hiv_reception_encounter
+
+end
+
+def create_vitals_encounter
+
+end
+
+def create_give_drugs_encounter
+
+end
+
+def create_outcome_encounter
+
+end
+
+def create_art_visit
+
+end
+
+
+start
